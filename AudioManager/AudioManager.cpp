e15625fea76f9b03a76457eb84d431d97fef19e2
Kasper Esbjörnsson <stades1@hermods.local>
2017-01-30 16:13:11 +0100

    Fixed cross fading and channel volumes

--- AudioManager/AudioManager.cpp
+++ AudioManager/AudioManager.cpp
@@ -14,6 +14,9 @@ void AudioManager::Init()
 	
 	myTryResults = myAudioSystem->initialize(myMaxChannels, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0);
 	assert(myTryResults == FMOD_OK && "Error initializing audio system");
+
+	SetCategoryVolume(AudioCategory::Effect, 100);
+	SetCategoryVolume(AudioCategory::Music, 100);
 }
 
 void AudioManager::Update()
@@ -37,6 +40,11 @@ void AudioManager::Update()
 			{
 				UnloadAudioFile(mySounds[index].myName);
 			}
+		}
+
+		if (mySounds[index].myIsCrossFading)
+		{
+			CrossFadeUpdate(index);
 		}
 	}
 
@@ -63,7 +71,7 @@ void AudioManager::LoadAudioBank(const std::string & aAudioBankName, bool aIsMas
 	}
 }
 
-void AudioManager::LoadAudioFile(const std::string& aAudioName)
+void AudioManager::LoadAudioFile(const std::string& aAudioName, AudioCategory aCategory)
 {
 	std::string eventName = "event:/";
 	eventName += aAudioName;
@@ -79,6 +87,7 @@ void AudioManager::LoadAudioFile(const std::string& aAudioName)
 
 		mySounds[myUsedAudioFiles].mySoundInstances = tempInstance;
 		mySounds[myUsedAudioFiles].myName = aAudioName;
+		mySounds[myUsedAudioFiles].myCategory = aCategory;
 		myUsedAudioFiles++;
 	}
 }
@@ -109,28 +118,68 @@ void AudioManager::UnloadAudioFile(const std::string & aAudioName)
 	assert(false && "Could not unload audio file. Check name.");
 }
 
-void AudioManager::Play(const std::string& aAudioName, bool aNewInstance, bool aShouldRepeat, float aVolumePercentage)
+void AudioManager::CrossFade(const std::string & aAudioOne, const std::string & aAudioTwo, float aCrossFadeScalar)
 {
-	if (aNewInstance)
+	bool foundAudioOne = false;
+	bool foundAudioTwo = false;
+	for (unsigned int index = 0; index < myUsedAudioFiles; ++index)
 	{
-		LoadAudioFile(aAudioName);
-		mySounds[myUsedAudioFiles - 1].mySoundInstances->setVolume(aVolumePercentage / 100.f);
-		mySounds[myUsedAudioFiles - 1].myShouldBeFreed = true;
-		mySounds[myUsedAudioFiles - 1].mySoundInstances->start();
-	}
-	else
-	{
-		for (unsigned int i = 0; i < (myUsedAudioFiles); ++i)
+		if (!foundAudioOne)
 		{
-			if (mySounds[i].myName == aAudioName)
+			if (mySounds[index].myName == aAudioOne)
 			{
-				mySounds[i].myIsRepeating = aShouldRepeat;
-				mySounds[i].mySoundInstances->setVolume(aVolumePercentage / 100.f);
-				mySounds[i].mySoundInstances->start();
-				break;
+				mySounds[index].myIsCrossFading = true;
+				mySounds[index].myIsCrossFadingUp = false;
+				mySounds[index].myCrossFadeScalar = aCrossFadeScalar;
+				foundAudioOne = true;
 			}
 		}
+		
+		if (!foundAudioTwo)
+		{
+			if (mySounds[index].myName == aAudioTwo)
+			{
+				mySounds[index].myIsCrossFading = true;
+				mySounds[index].myIsCrossFadingUp = true;
+				mySounds[index].myCrossFadeScalar = aCrossFadeScalar;
+				foundAudioTwo = true;
+				Play(aAudioTwo,  false, 0.f);
+			}
+		}
+
+		if (foundAudioOne && foundAudioTwo)
+		{
+			break;
+		}
 	}
+}
+
+void AudioManager::Play(const std::string& aAudioName, bool aShouldRepeat, float aVolumePercentage)
+{
+	for (unsigned int i = 0; i < (myUsedAudioFiles); ++i)
+	{
+		if (mySounds[i].myName == aAudioName)
+		{
+			aVolumePercentage = CalculatePlayVolume(aVolumePercentage, mySounds[i].myCategory);
+			mySounds[i].myIsRepeating = aShouldRepeat;
+			mySounds[i].mySoundInstances->setVolume(aVolumePercentage);
+			mySounds[i].mySoundInstances->start();
+			break;
+		}
+	}
+}
+
+void AudioManager::PlayNewInstance(const std::string & aAudioName, AudioCategory aCategory, bool aShouldRepeat, float aVolumePercentage)
+{
+	LoadAudioFile(aAudioName);
+
+	mySounds[myUsedAudioFiles - 1].myCategory = aCategory;
+
+	aVolumePercentage = CalculatePlayVolume(aVolumePercentage);
+
+	mySounds[myUsedAudioFiles - 1].mySoundInstances->setVolume(aVolumePercentage);
+	mySounds[myUsedAudioFiles - 1].myShouldBeFreed = true;
+	mySounds[myUsedAudioFiles - 1].mySoundInstances->start();
 }
 
 void AudioManager::Stop(const std::string & aAudioName)
@@ -153,6 +202,50 @@ void AudioManager::StopAll()
 	}
 }
 
+void AudioManager::SetMasterVolume(float aMasterVolume)
+{
+	myMasterVolume = aMasterVolume / 100;
+	SetCategoryVolume(AudioCategory::Effect, myEffectsVolume * 100);
+	SetCategoryVolume(AudioCategory::Music, myMusicVolume * 100);
+}
+
+void AudioManager::SetCategoryVolume(AudioCategory aCategory, float aVolumePercentage)
+{
+	if (myMasterVolume > 1 && aVolumePercentage > 1)
+	{
+		aVolumePercentage = (myMasterVolume / 100.f) * (aVolumePercentage / 100.f);
+	}
+	else
+	{
+		aVolumePercentage = myMasterVolume * (aVolumePercentage / 100.f);
+	}
+
+	switch (aCategory)
+	{
+	case AudioCategory::Effect:
+		myEffectsVolume = aVolumePercentage;
+		break;
+	case AudioCategory::Music:
+		myMusicVolume = aVolumePercentage;
+		break;
+	default:
+		assert(false && "Bad AudioCategory to set volume for");
+		break;
+	}
+}
+
+void AudioManager::SetRepeat(const std::string & aAudioName, bool aRepeatState)
+{
+	for (unsigned int index = 0; index < myUsedAudioFiles; ++index)
+	{
+		if (mySounds[index].myName == aAudioName)
+		{
+			mySounds[index].myIsRepeating = aRepeatState;
+			break;
+		}
+	}
+}
+
 AudioManager::AudioManager()
 {
 }
@@ -160,4 +253,63 @@ AudioManager::AudioManager()
 AudioManager::~AudioManager()
 {
 	myAudioSystem->release();
+}
+
+void AudioManager::CrossFadeUpdate(unsigned int aIndex)
+{
+	float newVolume = 0;
+	mySounds[aIndex].mySoundInstances->getVolume(&newVolume);
+
+	if (mySounds[aIndex].myIsCrossFadingUp)
+	{
+		newVolume += mySounds[aIndex].myCrossFadeScalar;
+	}
+	else if (!mySounds[aIndex].myIsCrossFadingUp)
+	{
+		newVolume -= mySounds[aIndex].myCrossFadeScalar;
+	}
+
+	mySounds[aIndex].mySoundInstances->setVolume(newVolume);
+
+	float volumeToReach = myMasterVolume;
+
+	switch (mySounds[aIndex].myCategory)
+	{
+	case AudioCategory::Effect:
+		volumeToReach = myEffectsVolume;
+		break;
+	case AudioCategory::Music:
+		volumeToReach = myMusicVolume;
+		break;
+	default:
+		volumeToReach = myMasterVolume;
+		break;
+	}
+
+	if (newVolume <= 0.f || newVolume >= volumeToReach)
+	{
+		if (!mySounds[aIndex].myIsCrossFadingUp)
+		{
+			Stop(mySounds[aIndex].myName);
+		}
+
+		mySounds[aIndex].myIsCrossFading = false;
+		mySounds[aIndex].myIsCrossFadingUp = false;
+	}
+}
+
+float AudioManager::CalculatePlayVolume(float aPercentage, AudioCategory aCategory)
+{
+	if (aCategory == AudioCategory::Effect)
+	{
+		return (aPercentage / 100.f) * myEffectsVolume;
+	}
+	else if (aCategory == AudioCategory::Music)
+	{
+		return (aPercentage / 100.f) * myMusicVolume;
+	}
+	else
+	{
+		return (aPercentage / 100.f) * (myMasterVolume);
+	}
 }
--- AudioManager/AudioManager.h
+++ AudioManager/AudioManager.h
@@ -2,13 +2,24 @@
 #include "FMOD/fmod_studio.hpp"
 #include <assert.h>
 #include <string>
+	
+enum class AudioCategory
+{
+	None,
+	Effect,
+	Music,
+};
 
 struct Sound
 {
 	FMOD::Studio::EventInstance* mySoundInstances;
 	std::string myName;
+	AudioCategory myCategory;
 	bool myShouldBeFreed = false;
 	bool myIsRepeating;
+	bool myIsCrossFading = false;
+	bool myIsCrossFadingUp = false;
+	float myCrossFadeScalar = 0.f;
 };
 
 struct Bank
@@ -26,20 +37,33 @@ public:
 	void Init();
 	void Update();
 	void LoadAudioBank(const std::string& aBankName, bool aIsMaster = false);
-	void LoadAudioFile(const std::string& aAudioName);
+	void LoadAudioFile(const std::string& aAudioName, AudioCategory aCategory = AudioCategory::None);
 	void UnloadAudioFile(const std::string& aAudioName);
-	void Play(const std::string& aAudioName, bool aNewInstance = false, bool aShouldRepeat = false, float aVolumePercentage = 100);
+	void CrossFade(const std::string& aAudioOne, const std::string& aAudioTwo, float aCrossFadeScalar);
+	void Play(const std::string& aAudioName, bool aShouldRepeat = false, float aVolumePercentage = 100);
+	void PlayNewInstance(const std::string& aAudioName, AudioCategory aCategory = AudioCategory::None, bool aShouldRepeat = false, float aVolumePercentage = 100);
 	void Stop(const std::string& aAudioName);
 	void StopAll();
+	void SetMasterVolume(float aMasterVolume);
+	void SetCategoryVolume(AudioCategory aCategory, float aVolume);
+	void SetRepeat(const std::string& aAudioName, bool aRepeatState);
 private:
 	AudioManager();
 	~AudioManager();
 	
+	void CrossFadeUpdate(unsigned int aIndex);
+
+	float CalculatePlayVolume(float aPercentage, AudioCategory aCategory = AudioCategory::None);
+
 	static const int myMaxAudioFiles = 100;
 	static const int myMaxBanks = 50;
 	const int myMaxChannels = 512;
 	unsigned int myUsedAudioFiles = 0;
 	unsigned int myUsedBanks = 0;
+
+	float myMasterVolume = 100;
+	float myEffectsVolume = 100;
+	float myMusicVolume = 100;
 
 	Sound mySounds[myMaxAudioFiles];
 	Bank myBanks[myMaxBanks];
